b3126a1eaff1f00a38bf2a9e5780774b40ab125e
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-04-16 22:33:34 -0700

    fix colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,14 +2,14 @@ module.exports = {
   mode: "jit",
   purge: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
-    extend: {},
-    colors: {
-      // TODO: Your colors here
-      main: {
-        100: "hsl(243, 100%, 93%)",
-        200: "hsl(229, 7%, 55%)",
-        300: "hsl(228, 56%, 26%)",
-        400: "hsl(229, 57%, 11%)",
+    extend: {
+      colors: {
+        main: {
+          100: "hsl(243, 100%, 93%)",
+          200: "hsl(229, 7%, 55%)",
+          300: "hsl(228, 56%, 26%)",
+          400: "hsl(229, 57%, 11%)",
+        },
       },
     },
     fontFamily: {
